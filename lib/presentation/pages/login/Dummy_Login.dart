5f89489ef8c6f6446bb64784463197a420f79d97
r3nyah <arianfebrian46@gmail.com>
2023-01-24 11:09:33 +0700

    new login page

---
+++ lib/presentation/pages/login/Dummy_Login.dart
@@ -0,0 +1,34 @@
+import 'package:cafe_wikusama/app.dart';
+import 'package:flutter/material.dart';
+
+class LoginDummy extends StatefulWidget {
+  const LoginDummy({super.key});
+
+  @override
+  State<LoginDummy> createState() => _LoginDummyState();
+}
+
+class _LoginDummyState extends State<LoginDummy> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/image/common/Logo_Background.png'
+            )
+          )
+        ),
+        child: Column(children: [
+          Image.asset('assets/image/common/Login_Logo.png'),
+          Container(
+            height: 500,
+            color: Colors.green,
+            
+          )
+        ]),
+      ),
+    );
+  }
+}
