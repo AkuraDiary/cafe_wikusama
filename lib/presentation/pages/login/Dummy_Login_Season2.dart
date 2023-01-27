02929a6b5809dc6224ee799f468df98bb0c8404c
r3nyah <arianfebrian46@gmail.com>
2023-01-27 10:33:30 +0700

    UI Changed

--- lib/presentation/pages/login/Dummy_Login_Season2.dart
+++ lib/presentation/pages/login/Dummy_Login_Season2.dart
@@ -43,21 +43,19 @@ class _LoginState extends State<Login> {
         child: Container(
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.green.shade900,
-                Colors.green,
-                Colors.green.shade400,
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.centerRight,
-            )
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                  'https://s3-alpha-sig.figma.com/img/5910/ec35/a763731970db1d8a019c7d83e29aa619?Expires=1675641600&Signature=bxO3kIdx9e7fOgS~TE1-42irCn3UjkF6crtC2Sx1~7c20N2dLhRyc-82NJrRnpoQCm9kOjVo01zKCaf4BMfLg~KUCiHR~Mwo3DS9hC6tA9n-wlYsMCZnWRagXNW852ZszAMR5xeGoJT2JOEqEEZNLqwsIw15qluQsUUbNQoj~p7jvWmS5csQQGlfTzdVIaDdzgFhsTB24ov2Ip0f7CMvv4HLk4Oz1N0QyDxOAt~3MoQRwSgmXTc6mTHwSI1BICi7Jfv~6CLbDNvg2vi-wv74KeBZRKhsXgd4ogdX96U1LbevJrvP2keZfwb4sxrpTo370ZYB0km0ZbCa81JIttcAjw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+                ),
+              scale: 1.5,
+              fit: BoxFit.fitHeight,
+            ),
           ),
           child: Column(
             children: [
               Container(
-                height: 210,
+                height: 450,
                 padding: const EdgeInsets.symmetric(
                   horizontal: 20,
                   vertical: 35,
@@ -66,21 +64,6 @@ class _LoginState extends State<Login> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: const [
-                    Text(
-                      'Login',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 32,
-                      ),
-                    ),
-                    SizedBox(height: 7.5,),
-                    Text(
-                      'Welcome Back',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 32,
-                      ),
-                    ),
                   ],
                 ),
               ),
