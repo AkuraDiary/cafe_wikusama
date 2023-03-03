3fe61cb3340f890358f8874c2633eae35980a904
r3nyah <arianfebrian46@gmail.com>
2023-03-03 14:28:29 +0700

    edit food list

--- lib/presentation/pages/Admin/Food_List.dart
+++ lib/presentation/pages/Admin/Food_List.dart
@@ -340,32 +340,148 @@ class _FoodListState extends State<FoodList> {
               ),
             ])
           ]),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                user.addItem = !user.addItem;
-              });
-            },
-            child: AnimatedContainer(
-                height: 35,
-                width: 70,
-                duration: Duration(milliseconds: 300),
-                decoration: BoxDecoration(
-                    color:
-                    user.addItem ? Colors.transparent : Colors.transparent,
-                    borderRadius: BorderRadius.circular(5),
-                    border: Border.all(
-                      color: user.addItem
-                          ? Colors.transparent
-                          : Colors.transparent,
-                    )),
-                child: Center(
-                    child: Text(user.addItem ? 'Edited' : 'Edit',
+          TextButton(
+            child: Text(
+              'Edit',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+              ),
+            ),
+            onPressed: (){
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      backgroundColor: Colors.white,
+                      alignment: Alignment.center,
+                      title: Text(
+                        'Edit Food',
                         style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                            color:
-                            user.addItem ? Colors.black : Colors.black)))),
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 23
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      content: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.amber,
+                              fixedSize: const Size(100,100),
+                              shape: const CircleBorder(),
+                            ),
+                            onPressed: (){
+
+                            },
+                            child: const Text(
+                                '',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 32,
+                                )
+                              )
+                            ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              hintText: 'Food Name',
+                              enabled: true,
+                              contentPadding: const EdgeInsets.only(
+                                left: 14.0,
+                                bottom: 8.0,
+                                top: 8.0
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: new BorderSide(color: Colors.black),
+                                borderRadius: new BorderRadius.circular(10),
+                              ),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: new BorderSide(color: Colors.black),
+                                borderRadius: new BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          TextFormField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              hintText: 'Price',
+                              enabled: true,
+                              contentPadding: const EdgeInsets.only(
+                                left: 14.0,
+                                bottom: 8.0,
+                                top: 8.0
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: new BorderSide(color: Colors.black),
+                                borderRadius: new BorderRadius.circular(10),
+                              ),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: new BorderSide(color: Colors.black),
+                                borderRadius: new BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              hintText: 'Description',
+                              enabled: true,
+                              contentPadding: const EdgeInsets.only(
+                                left: 14.0,
+                                bottom: 8.0,
+                                top: 8.0
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: new BorderSide(color: Colors.black),
+                                borderRadius: new BorderRadius.circular(10),
+                              ),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: new BorderSide(color: Colors.black),
+                                borderRadius: new BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 45,
+                          ),
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: Container(
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(Colors.amber),
+                                ),
+                                child: Text(
+                                   'Submit'
+                                ),
+                                onPressed: (){
+
+                                },
+                              )
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  });
+            },
           )
         ],
       ),
