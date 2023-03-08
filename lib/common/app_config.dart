495f35ffdaf06eb9ba7293d15ce0ae6c1fdfce3d
r3nyah <arianfebrian46@gmail.com>
2023-03-08 15:16:42 +0700

    dio

---
+++ lib/common/app_config.dart
@@ -0,0 +1,11 @@
+class Configs{
+  // String flavor;
+  // Configs(this.flavor);
+  Configs();
+  static const baseUrlDev = 'https://api.zackym.com/';
+    static const midtransDev = '';
+    static const midtransProd = '';
+
+  // Backend: http://lms.aksamedia.co.id
+  // Client: https://lms-mulia.netlify.app
+}
