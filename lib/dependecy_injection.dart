e2d2f08f6561ddcfb94cf2facc379a94ea28490c
r3nyah <arianfebrian46@gmail.com>
2023-03-08 16:26:18 +0700

    dependecy

--- lib/dependecy_injection.dart
+++ lib/dependecy_injection.dart
@@ -1,3 +1,4 @@
+import 'package:cafe_wikusama/presentation/pages/Login/login_page.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 import 'package:dio/dio.dart';
@@ -5,6 +6,9 @@ import 'common/dio.dart';
 import 'common/app_config.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'common/app_shared_preferences.dart';
+import 'data/sources/remote/auth_remote_data_source.dart';
+import 'data/sources/repository/auth_repository_impl.dart';
+import 'domain/repositories/auth_repository.dart';
 
 final locator = GetIt.instance;
 
@@ -16,7 +20,23 @@ void init() {
   locator.registerLazySingleton<AppSharedPreferences>(
       () => AppSharedPreferences(preferences: locator()));
 
+  // use case section
+  locator.registerLazySingleton(
+      () => Login(locator(),));
+
   locator.registerSingletonAsync<Dio>(() => DioClient().client());
   locator.registerLazySingleton(() => http.Client());
   locator.registerLazySingleton(() => Configs());
+
+  //repository implementation section
+  locator.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(
+      authRemoteDataSource: locator(),
+    ),
+  );
+
+  //data source section
+  locator.registerLazySingleton<AuthRemoteDataSource>(
+    () => AuthRemoteDataSourceImpl(),
+  );
 }
